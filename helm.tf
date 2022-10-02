0f18bb927c18295e618bb996ffb70aef5d8bbce9
deggja <danieldagfinrud@gmail.com>
2022-10-02 15:30:02 +0200

    testing

---
+++ helm.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "argocd_applicationset" {
+  name       = "argocd-applicationset"
+  repository = https://argoproj.github.io/argo-helm
+  chart      = "argocd-applicationset"
+  namespace  = "argocd"
+  version    = "1.11.0"
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = https://argoproj.github.io/argo-helm
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  version    = "3.33.6"
+  values = [file("values.yaml")]
